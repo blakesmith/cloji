405c12e520614ce0dac7a964e8351dde6d8468ad
Blake Smith <blakesmith0@gmail.com>
2012-01-17 08:04:31 -0600

    Move palmdoc decompression algorithm into its own file

--- src/cloji/core.clj
+++ src/cloji/core.clj
@@ -1,5 +1,8 @@
 (ns cloji.core
-  (:import [java.util Date]))
+  (:require
+    [cloji.palmdoc :as palmdoc])
+  (:import
+    [java.util Date]))
 
 (defmacro with-location [l is body]
   `(do (.seek ~is ~l)
@@ -71,32 +74,8 @@
     (when (not (= 0 t))
       (doto (new Date) (.setTime (* 1000 t))))))
 
-(defn decomp-palmdoc
-  "Takes a stream of bytes compressed using the Palmdoc compression algorithm
-  and returns a sequence of decompressed bytes"
-  [c]
-  (loop [cs c us []]
-    (let [nc (first cs)]
-      (cond
-        (= nil nc) us
-        (= 0x00 nc) (recur (rest cs) (conj us nc))
-        (and (<= 0x01 nc) (>= 0x08 nc)) (recur (drop (+ 1 nc) cs) (into us (take nc (rest cs))))
-        (and (<= 0x09 nc) (>= 0x7F nc)) (recur (rest cs) (conj us nc))
-        (and (<= 0x80 nc) (>= 0xBF nc))
-          (if-let [nnc (nth cs 1 nil)]
-            (let [lz7 (bit-and 0x3FFF (bit-or (bit-shift-left nc 8) nnc))
-                  distance (bit-shift-right lz7 3)
-                  length (+ 3 (bit-and 7 lz7))
-                  lz7-decode (fn []
-                               (loop [coll us n 0]
-                                 (if (= n length)
-                                   coll
-                                   (recur (conj coll (nth coll (- (count coll) distance))) (inc n)))))]
-              (recur (drop 2 cs) (lz7-decode))))
-        (and (<= 0xC0 nc) (>= 0xFF nc)) (recur (rest cs) (into us [32 (bit-xor nc 0x80)]))))))
-
 (defn palmdoc-string [coll encoding]
-  (as-string (decomp-palmdoc coll) encoding))
+  (as-string (palmdoc/decomp-palmdoc coll) encoding))
 
 (defn huffman-string [coll encoding])
 
