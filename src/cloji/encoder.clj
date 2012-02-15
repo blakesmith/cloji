230172eac2cad22ce5f7f02796e956df68a92165
Blake Smith <blakesmith0@gmail.com>
2012-02-15 17:46:39 -0600

    Cut out the macro crap, get the simple small ascii case working

--- src/cloji/encoder.clj
+++ src/cloji/encoder.clj
@@ -23,40 +23,22 @@
                  2))))))
 
 (defn- type-c-compress [text offset]
-  (vector 2 (bit-xor (int (nth text offset)) 0x80)))
+  (vector 2 [(bit-xor (int (nth text offset)) 0x80)]))
 
 (defn- pass-through [text offset]
-  (vector 1 (nth text offset)))
+  (vector 1 [(int (nth text offset))]))
 
-;; (defmacro fall-through-chain [& forms]
-;;   "Evaluate a list of (pred) (body) forms, return the result of the first body where
-;; pred evaluates to true and body is not nil"
-;;   `(first (filter
-;;            #(not (nil? %))
-;;            (map
-;;             (fn [step#]
-;;               (let [[test# exp#] step#]
-;;                 (when (eval test#)
-;;                   (eval exp#))))
-;;             (partition 2 '~forms)))))
-
-(defmacro fall-through-chain [& clauses]
-  (when clauses
-    (list 'if (first clauses)
-          (if-let [exp# (and (next clauses) (second clauses))]
-            exp#
-            (throw (IllegalArgumentException. "Requires an even number of forms")))
-          (cons 'fall-through-chain (next (next clauses))))))
-            
-                             
 (defn- compression-chain [text us offset textlength]
-  (fall-through-chain 
-   (and (> offset 10) (> (- textlength offset) 10))
-   (type-b-compress (take offset text) offset)
-   (and (< (inc offset) textlength) (= \space (nth us (inc offset))))
-   (type-c-compress us offset)
-   (let [ch (nth text offset)] (or (= ch 0) (and (>= ch 9) (< ch 0x80))))
-   (pass-through us offset)))
+  (when (and (> offset 10) (> (- textlength offset) 10))
+    (when-let [comp (type-b-compress (take offset text) offset)]
+      comp))
+  (when (and (< (inc offset) textlength) (= \space (nth text (inc offset))))
+    (when-let [comp (type-c-compress us offset)]
+      comp))
+  (let [ch (int (nth text offset))]
+    (when (or (= ch 0) (and (>= ch 9) (< ch 0x80)))
+      (when-let [comp (pass-through text offset)]
+        comp))))
 
 (defn compressed-palmdoc [s charset]
   {:pre [(<= (count s) 4096)]}
--- test/cloji/test/encoder.clj
+++ test/cloji/test/encoder.clj
@@ -4,5 +4,7 @@
 
 (deftest compressed-palmdoc-impl
   (testing "exception raising with string sizes > 4096 bytes"
-    (is (thrown? java.lang.AssertionError (encoder/compressed-palmdoc (apply str (take 4097 (repeat \a))))))))
+    (is (thrown? java.lang.AssertionError (encoder/compressed-palmdoc (apply str (take 4097 (repeat \a))) "UTF-8"))))
+  (testing "pass through compression of simple strings"
+    (is (= [121 111 121 111 121 111] (encoder/compressed-palmdoc "yoyoyo" "UTF-8")))))
 
