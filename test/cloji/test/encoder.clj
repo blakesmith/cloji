0a9d9c5bd86f18c57841f079e50551758c3bbaab
Blake Smith <blakesmith0@gmail.com>
2012-02-16 08:54:45 -0600

    Add test for multi-byte characters

--- test/cloji/test/encoder.clj
+++ test/cloji/test/encoder.clj
@@ -6,5 +6,7 @@
   (testing "exception raising with string sizes > 4096 bytes"
     (is (thrown? java.lang.AssertionError (encoder/compressed-palmdoc (apply str (take 4097 (repeat \a))) "UTF-8"))))
   (testing "pass through compression of simple strings"
-    (is (= [121 111 121 111 121 111] (encoder/compressed-palmdoc "yoyoyo" "UTF-8")))))
+    (is (= [121 111 121 111 121 111] (encoder/compressed-palmdoc "yoyoyo" "UTF-8"))))
+  (testing "encoding UTF8 characters"
+    (is (= [-30 -128 -104] (encoder/compressed-palmdoc "\u2018" "UTF-8")))))
 
