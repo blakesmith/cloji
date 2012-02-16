076f6b0676ef2e3a6be0f72486732e3c0312df21
Blake Smith <blakesmith0@gmail.com>
2012-02-16 08:44:27 -0600

    Use actual byte chunking for characters

--- src/cloji/encoder.clj
+++ src/cloji/encoder.clj
@@ -4,6 +4,9 @@
         [clojure.contrib.seq-utils :only [find-first]])
   (:require [cloji.palmdoc :as palmdoc]))
 
+(defn- char-bytes [s offset charset]
+  (seq (.getBytes (subs s offset (inc offset)) charset)))
+
 (defn- type-b-compress [text offset]
   (if-let [matched-data (find-first
                          (fn [match-chunk]
@@ -22,30 +25,35 @@
                  (+ 0x8000 (bit-and (bit-shift-left m 3) 0x3ff8) (- chunk-size 3))
                  2))))))
 
-(defn- type-c-compress [text offset]
-  (vector 2 [(bit-xor (-> (nth text offset) int byte) 0x80)]))
+(defn- type-c-compress [text offset charset]
+  "Type C Compression - an ascii character followed by a space. Multibyte characters
+should return nil from this function"
+  (let [cb (char-bytes text offset charset)]
+    (when (= (count cb) 1)
+      (vector 2 (map #(bit-xor % 0x80) cb)))))
 
-(defn- pass-through [text offset]
-  (vector 1 [(-> (nth text offset) int byte)]))
+(defn- pass-through [text offset charset]
+  "Pass through, write the bytes straight to the compression stream"
+  (vector 1 (char-bytes text offset charset)))
 
-(defn- compression-chain [text offset textlength]
+(defn- compression-chain [text offset textlength charset]
   (when (and (> offset 10) (> (- textlength offset) 10))
     (when-let [comp (type-b-compress (take offset text) offset)]
       comp))
   (when (and (< (inc offset) textlength) (= \space (nth text (inc offset))))
-    (when-let [comp (type-c-compress text offset)]
+    (when-let [comp (type-c-compress text offset charset)]
       comp))
   (let [ch (int (nth text offset))]
     (when (or (= ch 0) (and (>= ch 9) (< ch 0x80)))
-      (when-let [comp (pass-through text offset)]
+      (when-let [comp (pass-through text offset charset)]
         comp))))
 
 (defn compressed-palmdoc [s charset]
   {:pre [(<= (count s) 4096)]}
   (let [textlength (count s)]
     (loop [cs [] offset 0]
-      (if-let [next-char (nth s offset nil)]
-        (let [[n chunk] (compression-chain s offset textlength)]
-          (recur (into cs chunk) (+ offset n)))
-        cs))))
-          
+      (if (< textlength (inc offset))
+        cs
+        (let [[n chunk] (compression-chain s offset textlength charset)]
+          (recur (into cs chunk) (+ offset n)))))))
+
