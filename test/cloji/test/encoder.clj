f457a42087c12ee6b5db8592093798cf44865e9c
Blake Smith <blakesmith0@gmail.com>
2012-02-18 17:44:41 -0600

    Additional empty byte test

--- test/cloji/test/encoder.clj
+++ test/cloji/test/encoder.clj
@@ -6,7 +6,8 @@
   (testing "exception raising with string sizes > 4096 bytes"
     (is (thrown? java.lang.AssertionError (encoder/compressed-palmdoc (apply str (take 4097 (repeat \a))) "UTF-8"))))
   (testing "pass through compression of simple ascii only strings"
-    (is (= [121 111 121 111 121 111] (encoder/compressed-palmdoc "yoyoyo" "UTF-8"))))
+    (is (= [121 111 121 111 121 111] (encoder/compressed-palmdoc "yoyoyo" "UTF-8")))
+    (is (= [0 0] (encoder/compressed-palmdoc "\u0000\u0000" "UTF-8"))))
   (testing "encoding UTF8 characters (type a compression)"
     (is (= [3 226 128 152] (encoder/compressed-palmdoc "\u2018" "UTF-8"))))
   (testing "type c encoding, character followed by a space"
