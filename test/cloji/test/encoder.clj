98016269c8b4cc5113a29efd20c30749874f8dd9
Blake Smith <blakesmith0@gmail.com>
2012-02-18 17:38:37 -0600

    More accurate test descriptions

--- test/cloji/test/encoder.clj
+++ test/cloji/test/encoder.clj
@@ -5,9 +5,9 @@
 (deftest compressed-palmdoc-impl
   (testing "exception raising with string sizes > 4096 bytes"
     (is (thrown? java.lang.AssertionError (encoder/compressed-palmdoc (apply str (take 4097 (repeat \a))) "UTF-8"))))
-  (testing "pass through compression of simple strings"
+  (testing "pass through compression of simple ascii only strings"
     (is (= [121 111 121 111 121 111] (encoder/compressed-palmdoc "yoyoyo" "UTF-8"))))
-  (testing "encoding UTF8 characters"
+  (testing "encoding UTF8 characters (type a compression)"
     (is (= [3 226 128 152] (encoder/compressed-palmdoc "\u2018" "UTF-8"))))
   (testing "type c encoding, character followed by a space"
     (is (= [244] (encoder/compressed-palmdoc " t" "UTF-8")))
