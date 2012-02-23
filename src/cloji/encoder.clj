73e91384f6b16c3e753c753ceec89299df7587bd
Blake Smith <blakesmith0@gmail.com>
2012-02-22 22:49:31 -0600

    Do everything at the byte level rather than character level, still need to optimize

--- src/cloji/encoder.clj
+++ src/cloji/encoder.clj
@@ -39,57 +39,60 @@ one does. Otherwise return the :else expression or nil"
         roff (if (> rend strlen) strlen rend)]
     (subs s offset roff)))
 
-(defn- type-a-compress [text offset charset]
-  (let [ss (get-subs text 1 offset)
-        cb (char-bytes ss charset)]
-    (vector 1 (into [(count cb)] cb))))
+(defn- type-a-compress [byte-sequence offset]
+  (let [cb (take-while
+            #(or
+              (and (<= 0x01 %) (>= 0x08 %))
+              (and (<= 0x80 %) (>= 0xff %)))
+            (if (< (count byte-sequence) (+ offset 8))
+              (subvec byte-sequence offset)
+              (subvec byte-sequence offset (+ offset 8))))]
+        (vector (count cb) (into [(count cb)] cb))))
 
-(defn- type-b-compress [text preamble offset charset]
-  (let [byte-sequence (char-bytes preamble charset)]
-    (when (= offset 2057) (prn (count preamble) (count byte-sequence)))
-    (if-let [matched-data (find-first
-                           (fn [match-chunk]
-                             (let [[match chunk] match-chunk]
-                               (and (>= match 0) (<= (- offset match) 2047))))
-                           (map
-                            (fn [i]
-                              (let [chunk (char-bytes (get-subs text i offset) charset)]
-                                (vector (index-of byte-sequence chunk) (count chunk))))
-                            (range 10 2 -1)))]
-      (let [[distance chunk-size] matched-data
-            m (- offset distance)]
-        (when (and (<= chunk-size 10) (>= chunk-size 3))
-          (when (= offset 2057) (prn matched-data))
-          (vector chunk-size
-                  (packed-int
-                   (+ 0x8000 (bit-and (bit-shift-left m 3) 0x3ff8) (- chunk-size 3))
-                   2)))))))
+(defn- type-b-compress [byte-sequence preamble offset]
+  (if-let [matched-data (find-first
+                         (fn [match-chunk]
+                           (let [[match chunk] match-chunk]
+                             (and (>= match 0) (<= (- offset match) 2047))))
+                         (map
+                          (fn [i]
+                            (let [chunk (subvec byte-sequence offset (+ i offset))]
+                              (vector (index-of preamble chunk) (count chunk))))
+                          (range 10 2 -1)))]
+    (let [[distance chunk-size] matched-data
+          m (- offset distance)]
+      (when (and (<= chunk-size 10) (>= chunk-size 3))
+        (when (= offset 2057) (prn matched-data))
+        (vector chunk-size
+                (packed-int
+                 (+ 0x8000 (bit-and (bit-shift-left m 3) 0x3ff8) (- chunk-size 3))
+                 2))))))
 
-(defn- type-c-compress [text offset charset]
+(defn- type-c-compress [byte-sequence offset]
   "Type C Compression - an ascii character followed by a space. Multibyte characters
 should return nil from this function"
-  (let [cb (char-bytes (get-subs text 1 offset) charset)]
-    (when (and (= 1 (count cb)) (and (>= (first cb) 0x40) (< (first cb) 0x80)))
-      (vector 2 [(bit-xor (first cb) 0x80)]))))
+  (let [ch (nth byte-sequence offset)]
+    (when (and (>= ch 0x40) (< ch 0x80))
+      (vector 2 [(bit-xor ch 0x80)]))))
 
-(defn- pass-through [text offset charset]
+(defn- pass-through [byte-sequence offset]
   "Pass through, write the bytes straight to the compression stream"
-  (vector 1 (char-bytes (get-subs text 1 offset) charset)))
+  (vector 1 [(nth byte-sequence offset)]))
 
-(defn- compression-chain [text offset textlength charset]
+(defn- compression-chain [byte-sequence offset length charset]
    (condf
-    (and (> offset 10) (> (- textlength offset) 10)) (type-b-compress text (get-subs text (inc offset) 0) offset charset)
-    (and (< (inc offset) textlength) (= \space (nth text offset))) (type-c-compress text (inc offset) charset)
-    (let [ch (int (nth text offset))]
-      (or (= ch 0) (and (>= ch 9) (< ch 0x80)))) (pass-through text offset charset)
-    :else (type-a-compress text offset charset)))
+    (and (> offset 10) (> (- length offset) 10)) (type-b-compress byte-sequence (subvec byte-sequence 0 offset) offset)
+    (and (< (inc offset) length) (= \space (char (nth byte-sequence offset)))) (type-c-compress byte-sequence (inc offset))
+    (let [ch (nth byte-sequence offset)]
+      (or (= ch 0) (and (>= ch 9) (< ch 0x80)))) (pass-through byte-sequence offset)
+    :else (type-a-compress byte-sequence offset)))
 
 (defn compressed-palmdoc [s charset]
   {:pre [(<= (count s) 4096)]}
-  (let [textlength (count s)]
+  (let [byte-sequence (char-bytes s charset) length (count byte-sequence)]
     (loop [cs [] offset 0]
-      (if (< textlength (inc offset))
+      (if (< length (inc offset))
         cs
-        (let [[n chunk] (compression-chain s offset textlength charset)]
+        (let [[n chunk] (compression-chain byte-sequence offset length charset)]
           (recur (into cs chunk) (+ offset n)))))))
 
